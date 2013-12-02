84696c8e13f042b7d23c31ca84fa9752179f5490
Jean-Michel Didon <jeanmichel.didon@gmail.com>
2013-12-02 14:41:34 +0100

    Initialization

---
+++ PoKidEx/PoKidEx/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PoKidEx
+//
+//  Created by Jean-Michel Didon on 02/12/2013.
+//  Copyright (c) 2013 HETIC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PoKidEx/PoKidEx/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  PoKidEx
+//
+//  Created by Jean-Michel Didon on 02/12/2013.
+//  Copyright (c) 2013 HETIC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
